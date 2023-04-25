96db0d7ef1e9abdd9b0e69b5183be1a2c52bfa34
Pilomas <alvinjmini@live.com>
2023-04-25 20:35:44 -0300

    Update 44

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,21 @@
-console.log("calculando orçamento")
+document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
+document.querySelector("#telemetria").addEventListener("change", calcularOrcamento)
+document.querySelector("#mang_sim").addEventListener("change", calcularOrcamento)
+document.querySelector("#mang_nao").addEventListener("change", calcularOrcamento)
+document.querySelector("#serverless").addEventListener("change", calcularOrcamento)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
 
-document.querySelector("#QtdDados").addEventListener("change", CalcularOrcamento)
-document.querySelector("#SN").addEventListener("change", CalcularOrcamento)
-document.querySelector("#AutoGerido").addEventListener("change", CalcularOrcamento)
-document.querySelector("#Serverless").addEventListener("change", CalcularOrcamento)
-document.querySelector("#Ctrl3").addEventListener("change", CalcularOrcamento)
-document.querySelector("#prazo").addEventListener("change", CalcularOrcamento)
-
-
-function CalcularOrcamento(){
+function calcularOrcamento() {
     
-    let preco = QtdDados.value*0.50
-    
-    if (SH.checked)  preco*=1.1
+    let preco = qtde.value * 100
+    if (js.checked) preco *= 1.1
+    if(mang_sim.checked) preco += 500
+    if(serverless.checked) preco += 300
 
-    if(AutoGerido.checked) preco*=0.8
-    if(Serverless.checked) preco+=400
-    if(Ctrl3.checked) outuput.innerHTML="R$"+preco.toFixed(2)+" + R$50.00 Mensais"
-
-    let taxaDeUrgencia = preco * (1.1-prazo.value * 0.1)
+    let taxaDeUrgencia = preco * (1.1 - prazo.value * 0.1)
     preco += taxaDeUrgencia
-    label_prazo.innerhtml=`Prazo (${prazo.value} semanas)`
 
-    output.innerHTML ="R$"+preco.toFixed(2)
+    label_prazo.innerHTML = `Prazo (${prazo.value} semanas)`
 
+    output.innerHTML = "R$ " + preco.toFixed(2)
 }
